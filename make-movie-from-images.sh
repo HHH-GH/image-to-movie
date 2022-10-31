8af0f5bcd14b6bcecde9369f26a914826f055920
HHH-GH <hayden@opie-etc.com>
2022-10-31 21:18:11 +0800

    Quick tidy of code and comments

--- make-movie-from-images.sh
+++ make-movie-from-images.sh
@@ -33,12 +33,13 @@ command -v "${IM_IDENTIFY}"  >/dev/null 2>&1 || { echo >&2 "ERROR: ImageMagick i
 command -v "${IM_MAGICK}"  >/dev/null 2>&1 || { echo >&2 "ERROR: ImageMagick majick command is not available. Is ImageMagick installed properly? Is the .env file set correctly?"; exit 1; }
 command -v "${FF_FFMPEG}"  >/dev/null 2>&1 || { echo >&2 "ERROR: FFmpeg ffmpeg command is not available. Is FFmpeg installed properly? Is the .env file set correctly?"; exit 1; }
 
+
 # 3. Set program defaults
 
-# TODO(HHH-GH): this?
-# https://dwheeler.com/essays/filenames-in-shell.html
-# 3.2 Set IFS to just newline and tab at the start of each script
-# IFS means the Field Separators
+# Set Field Separators
+# as per '3.2 Set IFS to just newline and tab at the start of each script'
+# at https://dwheeler.com/essays/filenames-in-shell.html
+# (IFS means the Field Separators)
 # Setting it to just \n (new lines) and \t (tab) means that the script
 # should be able to handle file names with spaces in them
 # With this setting, though, any arguments passed to commands as flags
@@ -47,18 +48,10 @@ command -v "${FF_FFMPEG}"  >/dev/null 2>&1 || { echo >&2 "ERROR: FFmpeg ffmpeg c
 # - The 0.5x0.5 etc would be counted as part of the -h
 IFS="$(printf '\n\t')"
 
-
 # Folder locations
 readonly IMG_SOURCE_DIR='./source'
 readonly IMG_OUTPUT_DIR='./output'
 readonly IMG_TEST_SOURCE_DIR='./test'
-
-# Temp file that holds a list of processed images to make into a movie
-# Holds a list of images to concatenate into a movie
-# https://trac.ffmpeg.org/wiki/Concatenate
-# Created during processing
-# Gets deleted during clean up
-readonly PROCESSED_IMG_FILE_LIST='list.txt'
 
 # Output settings
 readonly VID_OUTPUT_WIDTH='720' 
@@ -74,7 +67,7 @@ readonly IMG_PROCESSING_JPG_QUALITY="100"						# -quality "${IMG_PROCESSING_COLO
 readonly IMG_PROCESSING_INTERPOLATE="bilinear"					# -interpolate "${IMG_PROCESSING_INTERPOLATE}" Colour sampling when sized down bilinear or catrom (bicubic)
 readonly IMG_PROCESSING_SATURATION="100,120,100"				# -modulate "${IMG_PROCESSING_SATURATION}" The middle number is the saturation i.e. 120 = 20%
 
-# 4. The actual program TODO(HHH-GH): all of it (printing defaults & versions, quit and clean up, image and movie processing)
+# 4. The actual program functions TODO(HHH-GH): tidy the code
 
 #######################################
 # Print the menu options.
@@ -134,8 +127,6 @@ function print_program_menu(){
 #	make_img_output_dir/tmp and the images
 #	inside it
 #######################################
-
-
 images_to_movie(){
 
 	# Make sure there are 5 arguments
@@ -172,15 +163,15 @@ images_to_movie(){
 	# shell https://dwheeler.com/essays/filenames-in-shell.html 
 	
 	# Look for files in make_img_src_dir and then 
-	# 1. Loop through, resize/etc, save to the tmp location
-	# 2. Use the files in the tmp location to make a movie
-	#	 and save the movie in the output location
-	# 3. Delete the tmp files and folder
-	# 4. Print a success message that says the name of the 
-	#	 output file
+	# 4A. Loop through, resize/etc, save to the tmp location
+	# 4B. Use the files in the tmp location to make a movie
+	#	  and save the movie in the output location
+	# 4C. Delete the tmp files and folder
+	# 4D. Print a success message that says the name of the 
+	#	  output file
 	
 	
-	# 1.
+	# 4A.
 	# Loop through the files in make_img_src_dir, resizing and 
 	# positioning on a canvas that is sized 
 	# 'make_vid_output_width × make_vid_output_height'
@@ -206,7 +197,6 @@ images_to_movie(){
 	mkdir -p "${make_img_output_tmp_dir}"  # -p means that it doesn't write an error if the folder exists
 	
 	# Loop through img_arr and process the files	
-	
 	local i=1
 	for infile in ${img_arr[*]}; do
 		
@@ -224,15 +214,9 @@ images_to_movie(){
 			echo -en "."
 		fi
 		
-		# Detect image size
-		# -quiet so it doesn't give warnings
-		# Put the src dir in front of the image name "${make_img_src_dir}"/"${infile}"
-		#local source_image_width=$("${IM_IDENTIFY}" -quiet -format "%w" "${make_img_src_dir}"/"${infile}")
-		#local source_image_height=$("${IM_IDENTIFY}" -quiet -format "%h" "${make_img_src_dir}"/"${infile}")
-		
 		# Write the string for the resize part		
 		# What should happen here
-		# The image should maintain it's proportions and aspect ratio
+		# The image should maintain its proportions and aspect ratio
 		# Can't be wider than VID_OUTPUT_WIDTH
 		# Can't be taller than VID_OUTPUT_WIDTH
 		# According to www.imagemagick.org/Usage/resize/
@@ -240,17 +224,16 @@ images_to_movie(){
 		# this will happen automatically, and the image will only be resized
 		# if it is larger than those dimensions (that's the \> part)
 		# The image is saved in $make_img_output_tmp_dir
-		# The image name is converted to lower case for convenience in our glob match later
+		# The image name is converted to lower case for convenience
+		# (See below where `cat` is used to pipe all jpgs to `ffmpeg`)
 		
-		#TODO(HHH-GH): figure out how to pass the unsharp arguments/etc as variables
-		
-		# Lower case the file name for convenience in our glob match later
+		# Lower case the file name
 		# the ,, part works in Bash 4
 		# or a more compatible way like
 		# local lowercase_infile=`echo "$infile" | tr '[:upper:]' '[:lower:]'` 
 		local lowercase_infile="${infile,,}"
 			
-		
+		# Process and save the image
 		"${IM_CONVERT}" -quiet \
 		"${make_img_src_dir}"/"${infile}" \
 		-auto-orient \
@@ -265,6 +248,7 @@ images_to_movie(){
 		
 		# Increment the counter
 		(( i++ ))
+		
 	done
 	
 	# Clean up from processing the images loop
@@ -273,14 +257,13 @@ images_to_movie(){
 	unset img_count
 		
 	
-	# 2.
+	# 4B.
 	# Make a movie from those files using make_vid_output_fps
-	# Movie name like this, with timestamp and fps and size tags so they're unique 
+	# TODO(HHH-GH): Movie name like this, with timestamp and fps and size tags so they're unique 
 	# e.g. `202209161139_8fps_720w_720h.mp4`
 	
 	echo -en "\n\t2/3: Turning the images into a movie\n"
 	
-	# Are there any processed images to turn into a movie?
 	# Get all the jpgs	
 	# This gets the file name of the image, without the folder name
 	local processed_img_arr=(`ls ${make_img_output_tmp_dir} | grep -i '.jpg'`)
@@ -300,49 +283,44 @@ images_to_movie(){
 	# Leaving this comment for reference
 	# Globbing not supported on Windows? 
 	# Also leaving in this for reference
-	# So have to pass it in from a pipe
+	# So have to pass it in from a pipe	
 	
-	
-	
-	# TODO(HHH-GH): the actual steps for making the movie
-	# Processing the image steps
-	# Might want to put making the black background into the resizing part instead of here
-	# or I can create a temp file to use as one of the inputs
-	# or the first input is the black background
-	# But how to calculate the position?
-	# Can I just use a crop and is there an option to center it?
-	# https://ffmpeg.org/ffmpeg-filters.html#crop
-	# -filter:v "crop=w:h:x:y" or -vf "crop=10:10:0:0"  https://bytexd.com/ffmpeg-how-to-crop-videos-images-using-the-crop-filter/
-	# "If x and y are not specified, the command will default to start cropping from the center"
-	# "FFmpeg gets the original input video width (iw) and height (ih) values automatically, so you can perform mathematical 
-	# operations using those values (e.g. iw/2 for half the input video width, or ih-100 to subtract 100 pixels from the input video height).
-	# https://www.linuxuprising.com/2020/01/ffmpeg-how-to-crop-videos-with-examples.html
-	#
-	# So
-	# Draw a box as one input
-	# Glob the files as the other input
-	# Layer the files on top of the box
-	# Scaling to fit the images in
-	# https://superuser.com/questions/547296/resizing-videos-with-ffmpeg-avconv-to-fit-into-static-sized-player/1136305#1136305
-	
-	
-	#echo "${FF_FFMPEG}" -framerate "${make_vid_output_fps}" -vcodec mjpeg -pattern_type glob -i "${make_img_output_tmp_dir}"/*.jpg -r 30 -c:v libx264 -crf 17 -pix_fmt yuv420p "${make_img_output_dir}"/test.mp4 -hide_banner -loglevel error
-	cat "${make_img_output_tmp_dir}"/*.jpg | "${FF_FFMPEG}" -framerate "${make_vid_output_fps}" -vcodec mjpeg -f image2pipe -i - -vf "scale=${make_vid_output_width}:${make_vid_output_height}:force_original_aspect_ratio=decrease:eval=frame,pad=${make_vid_output_width}:${make_vid_output_height}:-1:-1:color=black" -r 30 -c:v libx264 -crf 17 -pix_fmt yuv420p "${make_img_output_dir}"/"${make_vid_output_filename}".mp4 -hide_banner -loglevel error
 		
-	# Just draw a box
-	#"${FF_FFMPEG}" -i color=c=black:s="${make_vid_output_width}"x"${make_vid_output_height}":r="${make_vid_output_fps}" test.mp4 -hide_banner
-	
-	echo -en "\n\tMovie saved as '${make_vid_output_filename}.mp4' in '${make_img_output_dir}'\n"
+	# The command to turn all the images into a movie
+	# Because the images are already sized to fit into the box made by
+	# ${make_vid_output_width} and ${make_vid_output_height} the FFmpeg 
+	# video filter `scale` option works fine to fit them into the video
+	# as per https://superuser.com/questions/547296/resizing-videos-with-ffmpeg-avconv-to-fit-into-static-sized-player/1136305#1136305
+	# Use `force_original_aspect_ratio=decrease` so that the images passed in are 
+	# not sized up if smaller than the video dimensions
+	# Use `eval=frame` to cope with different-sized images coming in; each image is centered horizontally 
+	# and vertically according to its size vs the size of the video
+	# Use `pad` to make a background colour for the video that's shown when an image doesn't fill the whole
+	# space (i.e. letterboxing)
+	# FFmpeg Scale docs: https://ffmpeg.org/ffmpeg-filters.html#scale
+	# `cat` is used to pipe the images into `ffmpeg` (-f image2pipe) because Windows systems can't do glob pattern_type
+	# matching and bash can't send decimal values for frame duration into ffmpeg's concat demuxer 
+	# See Slideshow (http://trac.ffmpeg.org/wiki/Slideshow#Framerates) for examples
+	# FFmpeg's -hide_banner and -loglevel error flags are used to suppress any of FFmpeg's output text
+		
+	cat "${make_img_output_tmp_dir}"/*.jpg | "${FF_FFMPEG}" \
+	-framerate "${make_vid_output_fps}" \
+	-vcodec mjpeg \
+	-f image2pipe -i - \
+	-vf "scale=${make_vid_output_width}:${make_vid_output_height}:force_original_aspect_ratio=decrease:eval=frame,pad=${make_vid_output_width}:${make_vid_output_height}:-1:-1:color=black" \
+	-r 30 \
+	-c:v libx264 \
+	-crf 17 \
+	-pix_fmt yuv420p \
+	"${make_img_output_dir}"/"${make_vid_output_filename}".mp4 \
+	-hide_banner -loglevel error
 	
 	# Clean up from making the movies loop
 	unset processed_img_arr
-	unset processed_img_count
-	unset make_vid_output_filename
+	unset processed_img_count	
 	
-	
-	# 3.
+	# 4C.
 	# Delete make_img_output_tmp_dir and the files inside
-	# Keep the folder, only delete JPG files?
 	# Delete only JPG files, then delete the folder only if it is empty
 	
 	echo -en "\n\t3/3: Cleaning up the temporary files\n"
@@ -350,28 +328,27 @@ images_to_movie(){
 	# Delete JPG files in tmp
 	# https://superuser.com/questions/902064/how-to-recursivly-delete-all-jpg-files-but-keep-the-ones-containing-sample
 	# https://superuser.com/questions/654416/is-the-rm-buildin-gnu-command-case-sensitive
-	# find in the output temporary directory
+	# `find` in the output temp directory
 	# -maxdepth 1 only in that directory and not subdirectories
 	# Anything that matches (case-insensitive) '.jpg'
-	# and then delete it
+	# Delete any matches
 	find "${make_img_output_tmp_dir}/" -maxdepth 1 -iname '*.jpg' -delete
-	
-	# Delete the list of files
-	rm -f "${make_img_output_tmp_dir}"/"${PROCESSED_IMG_FILE_LIST}"
-	
-	# Delete tmp if empty
-	# Because we don't want to delete everything by accident
+		
+	# Delete tmp directory if empty
+	# Because we don't want to delete everything by accident if there is tomfoolery with directory names
 	# Suppress the error message if it is not empty
 	# https://unix.stackexchange.com/questions/387048/why-does-rmdir-p-ignore-fail-on-non-empty-fail-when-encountering-home
 	rmdir -p --ignore-fail-on-non-empty "${make_img_output_tmp_dir}/"
 	
 	
-	# 4.
-	# Print a success message
-	# Including the name of the movie and where it is located
+	# 4D.
+	# Print a success message, including the name of the movie and where it is located
 	
 	echo -en "\n\tFinished!\n"
+	echo -en "\n\tMovie saved as '${make_vid_output_filename}.mp4' in '${make_img_output_dir}'\n"
 	
+	# Last one to cleanup
+	unset make_vid_output_filename
 }
 
 #######################################
@@ -534,4 +511,4 @@ function main(){
 }
 
 # Run the program
-main
+main "$@"
